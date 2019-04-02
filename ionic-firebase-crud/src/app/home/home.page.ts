2d2cd8db20ee1962ed49397377a6ad2f3d199a60
CiaranMurphyGmit <35569736+CiaranMurphyGmit@users.noreply.github.com>
2019-04-02 03:30:42 +0100

    Created login, register pages

--- ionic-firebase-crud/src/app/home/home.page.ts
+++ ionic-firebase-crud/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonicModule, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  username:string;
+  password:string;
+
+  constructor(public navCtrl: NavController) { }
+
+  login(){
+    console.log("Username: " + this.username + ", Password: " + this.password);
+  }
+  register(){
+    this.navCtrl.navigateRoot('/register');
+  }
 }
