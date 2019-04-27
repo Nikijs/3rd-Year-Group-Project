315082ea2b30becfe5d67cdc00e7e1724bae0719
Nikijs <nikijs299@gmail.com>
2019-04-27 23:46:34 +0100

    BMI page complete & Health page changes

--- src/app/bmi/bmi.page.ts
+++ src/app/bmi/bmi.page.ts
@@ -12,6 +12,13 @@ import {AngularFireAuth} from '@angular/fire/auth';
 })
 export class BMIPage implements OnInit {
 
+  height:number;
+  weight:number;
+  age:number;
+  gender:string;
+  healtStatus: string;
+  
+  
   constructor(public afAuth: AngularFireAuth,
     public router: Router,
     public afstore: AngularFirestore,
@@ -19,6 +26,26 @@ export class BMIPage implements OnInit {
 
   ngOnInit() {
   }
+
+  calculateBmi(){
+   var bmi = (this.weight)/(this.height/100*this.height/100);
+   bmi = Math.round(bmi);
+
+    if(bmi < 18.5){
+     this.healtStatus = "You are underweight. BMI = " + bmi;
+    }
+    else if(bmi > 18.5 && bmi < 25){
+      this.healtStatus = "You have a normal weight. BMI =" + bmi;
+    }
+    else if(bmi > 30){
+	    this.healtStatus = "You are overweight. BMI = " + bmi;
+    }
+    else{
+	    this.healtStatus = "Error when calculating BMI." + bmi;
+    }
+    console.log(bmi , this.weight, this.height, this.age);
+  }
+  
   goBack(){
     this.router.navigateByUrl('/mainpage');
   }
--- src/app/health/health.page.ts
+++ src/app/health/health.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
 import { UserService } from '../user.service';
 import {firestore } from 'firebase/app'
 import {AngularFireAuth} from '@angular/fire/auth';
@@ -11,6 +11,11 @@ import {AngularFireAuth} from '@angular/fire/auth';
   styleUrls: ['./health.page.scss'],
 })
 export class HealthPage implements OnInit {
+  
+  slept:string;
+  water:string;
+  coffee:string;
+  exercise:string;
 
   constructor(public afAuth: AngularFireAuth,
     public router: Router,
@@ -19,7 +24,22 @@ export class HealthPage implements OnInit {
 
   ngOnInit() {
   }
+  accept(){
+    const slept = this.slept;
+    const water = this.water;
+    const coffee = this.coffee;
+    const exercise = this.exercise;
 
+    const posts = this.afstore.doc('posts/'+this.user.getUID()).set({
+    posts: firestore.FieldValue.arrayUnion({
+      slept,
+      water,
+      coffee,
+      exercise
+    })
+  })
+  
+  }
   goBack(){
     this.router.navigateByUrl('/mainpage');
   }
